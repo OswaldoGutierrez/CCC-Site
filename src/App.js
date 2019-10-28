1d3e2c1401451a5ef1c215fe471df065406000ae
OswaldoGutierrez <oswaldogutierrez143@gmail.com>
2019-10-28 13:18:48 -0700

    added links and simple content to each page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Hello</h1>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return <div className="about">Welcome to the About page</div>;
+  }
+}
+
+export default About;
