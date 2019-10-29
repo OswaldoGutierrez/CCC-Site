7d36827b1ab0ba52d0c173f7e7f19e2ae9e1b449
OswaldoGutierrez <oswaldogutierrez143@gmail.com>
2019-10-28 22:18:19 -0700

    added styles folder and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
-
-import "./App.css";
 
 function App() {
   return (
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,25 +1,41 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import contactImage from "../images/contact-image.jpg";
 
 export default function() {
   return (
-    <div className="Contact">
-      <div className="left-column">
-        <img src={contactImage} alt="pic" />
-      </div>
-
+    <div className="content">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactImage + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
       <div className="right-column">
         <div className="contact-points">
           <div className="points-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
             <div className="text">555-555-5555</div>
           </div>
 
           <div className="points-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
             <div className="text">example@sample.com</div>
           </div>
 
           <div className="points-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
             <div className="text">Talyorsville, UT</div>
           </div>
         </div>
