f81f7bf734b888ee457673cb93df775e5ed965e9
OswaldoGutierrez <oswaldogutierrez143@gmail.com>
2019-10-28 21:29:12 -0700

    added content to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,29 @@
 import React from "react";
 
-class Contact extends React.Component {
-  render() {
-    return <div className="Contact">Welcome to the Contact page</div>;
-  }
-}
+import contactImage from "../images/contact-image.jpg";
 
-export default Contact;
+export default function() {
+  return (
+    <div className="Contact">
+      <div className="left-column">
+        <img src={contactImage} alt="pic" />
+      </div>
+
+      <div className="right-column">
+        <div className="contact-points">
+          <div className="points-group">
+            <div className="text">555-555-5555</div>
+          </div>
+
+          <div className="points-group">
+            <div className="text">example@sample.com</div>
+          </div>
+
+          <div className="points-group">
+            <div className="text">Talyorsville, UT</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
