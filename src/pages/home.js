6a5ed689f9066586c5386b1ca51ff11c4c5794f1
OswaldoGutierrez <oswaldogutierrez143@gmail.com>
2019-10-29 20:26:04 -0700

    added a font

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,22 @@ import React from "react";
 
 class Home extends React.Component {
   render() {
-    return <div className="home">Welcome to Homepage</div>;
+    return (
+      <div className="home">
+        <img src="" alt="logo" />
+
+        <div className="services">
+          <h2>Services We Provide</h2>
+          <p>
+            something something something something something something
+            <br />
+            something something something something something something
+          </p>
+        </div>
+
+        <p>Request A Free Quote </p>
+      </div>
+    );
   }
 }
 
