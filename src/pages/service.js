73b4ae2f9eaa6ff7f74b62606e1fa1ff35dcaf24
OswaldoGutierrez <oswaldogutierrez143@gmail.com>
2019-10-30 16:01:33 -0700

    added some styles to service

--- src/pages/service.js
+++ src/pages/service.js
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
 
 export default class Serivce extends Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
   render() {
     return (
-      <div>
-        <form>
-          <div className="request-form">
+      <div className="one">
+        <form className="request-form">
+          <div className="inputs">
             <input type="text" placeholder="First Name, Last Name" />
             <input type="text" placeholder="Company" />
             <input type="email" placeholder="Email" />
             <input type="text" placeholder="Message" />
           </div>
-          <button type="submit">Request</button>
+          <button type="submit" className="btn">
+            Request
+          </button>
         </form>
       </div>
     );
