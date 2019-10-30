082595165380347203b1a85f8db81585602650e6
OswaldoGutierrez <oswaldogutierrez143@gmail.com>
2019-10-30 11:25:33 -0700

    update to service page

--- src/pages/service.js
+++ src/pages/service.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Service = () => {
-  return <div className="">Request Form</div>;
-};
+export default class Serivce extends Component {
+  constructor() {
+    super();
 
-export default Service;
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <div className="request-form">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" /> <br />
+            <input type="text" placeholder="Company" />
+            <input type="email" placeholder="Email" /> <br />
+            <input type="text" placeholder="Message" />
+          </div>
+          <button type="submit">Request</button>
+        </form>
+      </div>
+    );
+  }
+}
