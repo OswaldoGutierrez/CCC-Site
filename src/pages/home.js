9a66ed3c514da4d42dfcddaf70608224976cb882
OswaldoGutierrez <oswaldogutierrez143@gmail.com>
2019-10-30 10:26:27 -0700

    update to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        <img src="" alt="logo" />
+        <img src="" alt="Complete Commercial Cleaning" />
 
         <div className="services">
           <h2>Services We Provide</h2>
           <p>
-            something something something something something something
-            <br />
-            something something something something something something
+            Office Cleaning <br />
+            Floor Cleaning: <br />
+            (Strip & Wax, Wax, Rewax, Buff) <br />
+            Carpet Cleaning
           </p>
         </div>
 
-        <p>Request A Free Quote </p>
+        <div className="make-request">
+          <p>Something Something</p>
+          <Link to="/service">Estimate</Link>
+        </div>
       </div>
     );
   }
