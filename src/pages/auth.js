447a6923a18a5ec9c9e9bae9751628150860d298
OswaldoGutierrez <oswaldogutierrez143@gmail.com>
2019-11-04 20:57:15 -0800

    added styles to login

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Login from "../auth/login";
+import LoginImage from "../images/contact-image.jpg";
 
 export default class Auth extends Component {
   render() {
     return (
       <div className="auth-page-wrapper">
-        <div className="left-column"></div>
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${LoginImage})`
+          }}
+        />
 
         <div className="right-column">
           <Login />
